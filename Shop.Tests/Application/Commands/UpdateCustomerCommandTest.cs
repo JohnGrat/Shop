fea723bf3bda6da4e7e4cbc98f3c9514a98e6e3c
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-21 14:33:31 +0200

    added tests

--- Shop.Tests/Application/Commands/UpdateCustomerCommandTest.cs
+++ Shop.Tests/Application/Commands/UpdateCustomerCommandTest.cs
@@ -1,9 +1,4 @@
 ﻿using Shop.Application.Commands.UpdateCustomer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Shop.Tests.Application.Commands
