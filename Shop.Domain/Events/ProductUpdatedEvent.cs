5c3a80d22e7183e63e41bf8e6f77a312fc076499
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-21 13:42:51 +0200

    added product to domain

---
+++ Shop.Domain/Events/ProductUpdatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Shop.Domain.Events
+{
+    public class ProductUpdatedEvent : INotification
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public decimal Price { get; }
+
+        public ProductUpdatedEvent(Guid id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
