c31da94699d4fa3f1a9c62d30890d5dd767d4ba5
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-21 13:46:10 +0200

    product mapperly

---
+++ Shop.Application/Mapperly/ProductMapper.cs
@@ -0,0 +1,19 @@
+﻿using Riok.Mapperly.Abstractions;
+using Shop.Application.Commands.CreateProduct;
+using Shop.Application.ReadModels;
+using Shop.Domain.Events;
+using Shop.Shared.DTOs;
+
+
+namespace Shop.Application.Mapperly
+{
+    [Mapper]
+    public static partial class ProductMapper
+    {
+        public static partial ProductDTO ToDTO(ProductReadModel customer);
+        public static partial ProductDTO ToDTO(CreateProductCommand customer);
+        public static partial ProductReadModel ToDTO(ProductDTO customer);
+        public static partial ProductReadModel ToDTO(ProductCreatedEvent customer);
+        public static partial ProductReadModel ToDTO(ProductUpdatedEvent customer);
+    }
+}
