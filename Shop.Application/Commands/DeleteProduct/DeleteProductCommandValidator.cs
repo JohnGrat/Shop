5504cb51903655e69ce6b4f5f83b8c8441d43901
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-21 13:44:59 +0200

    added product, create, delete, update command handler

---
+++ Shop.Application/Commands/DeleteProduct/DeleteProductCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Shop.Application.Commands.DeleteProduct
+{
+    public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+    {
+        public DeleteProductCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}
