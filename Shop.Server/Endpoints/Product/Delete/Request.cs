1d69cd529d444a1932e603f07cc3298333be59d4
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-22 09:17:49 +0200

    added endpoints

---
+++ Shop.Server/Endpoints/Product/Delete/Request.cs
@@ -0,0 +1,7 @@
+﻿namespace Shop.Server.Endpoints.Product.Delete
+{
+    public class Request
+    {
+        public Guid Id { get; set; }
+    }
+}
--- Shop.Server/Endpoints/Product/GetAll/Handler.cs
+++ Shop.Server/Endpoints/Product/GetAll/Handler.cs
@@ -1,34 +1,34 @@
 ﻿using FastEndpoints;
 using MediatR;
-using Shop.Application.Queries.GetCustomers;
+using Shop.Application.Queries.GetProducts;
 using Shop.Server.DataTransferContracts;
 using Shop.Shared.DTOs;
 
-namespace Shop.Server.Endpoints.Customer.GetAll
+namespace Shop.Server.Endpoints.Product.GetAll
 {
-    public class Handler(ISender mediator) : Endpoint<EmptyRequest, ServiceResponse<IEnumerable<CustomerDTO>>>
+    public class Handler(ISender mediator) : Endpoint<EmptyRequest, ServiceResponse<IEnumerable<ProductDTO>>>
     {
         public override void Configure()
         {
-            Get("/api/v1/customers");
+            Get("/api/v1/products");
             AllowAnonymous();
         }
 
         public override async Task HandleAsync(EmptyRequest request, CancellationToken cancellationToken)
         {
-            ServiceResponse<IEnumerable<CustomerDTO>> result;
+            ServiceResponse<IEnumerable<ProductDTO>> result;
             try
             {
-                var customersDto = await mediator.Send(new GetCustomersQuery(), cancellationToken);
-                result = new ServiceResponse<IEnumerable<CustomerDTO>>
+                var productsDto = await mediator.Send(new GetProductsQuery(), cancellationToken);
+                result = new ServiceResponse<IEnumerable<ProductDTO>>
                 {
-                    Data = customersDto.ToList(),
+                    Data = productsDto.ToList(),
                     IsSuccess = true
                 };
             }
             catch (Exception ex)
             {
-                result = new ServiceResponse<IEnumerable<CustomerDTO>>
+                result = new ServiceResponse<IEnumerable<ProductDTO>>
                 {
                     ErrorMessage = ex.Message,
                     IsSuccess = false
