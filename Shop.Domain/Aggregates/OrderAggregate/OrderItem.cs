d137a14028105d22bc1b04407eea9d0df285024a
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-23 15:09:33 +0200

    added aggregate

---
+++ Shop.Domain/Aggregates/OrderAggregate/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Shop.Domain.Aggregates.OrderAggregate
+{
+    public class OrderItem 
+    {
+        public Guid ProductId { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal Price { get; private set; }
+
+
+        private OrderItem() { }
+
+        public OrderItem(Guid productId, int quantity, decimal price)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+
+
+        public void AddQuantity(int quantity)
+        {
+            Quantity += quantity;
+        }
+    }
+}
---
+++ Shop.Domain/Events/OrderStatusUpdatedEvent.cs
@@ -0,0 +1,18 @@
+﻿
+using MediatR;
+using Shop.Domain.Aggregates.OrderAggregate;
+
+namespace Shop.Domain.Events
+{
+    public class OrderStatusUpdatedEvent : INotification
+    {
+        public Guid Id { get; }
+        public OrderStatus OrderStatus { get; }
+
+        public OrderStatusUpdatedEvent(Guid id, OrderStatus orderStatus)
+        {
+            Id = id;
+            OrderStatus = orderStatus;
+        }
+    }
+}
