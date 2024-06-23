1ec3b5315aa4cd94772bade33723ddb853339d7a
JohnGrat <51702387+JohnGrat@users.noreply.github.com>
2024-06-23 15:10:53 +0200

    added CreateOrderCommand handler

---
+++ Shop.Application/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Shop.Application.Mapperly;
+using Shop.Domain.Events;
+using Shop.Domain.Repositories.Interfaces;
+
+namespace Shop.Application.EventHandlers
+{
+    public class OrderCreatedEventHandler : INotificationHandler<OrderCreatedEvent>
+    {
+        private readonly ISqlDbRepository _sqlDbRepository;
+
+        public OrderCreatedEventHandler(
+            ISqlDbRepository sqlDbRepository)
+        {
+            _sqlDbRepository = sqlDbRepository;
+        }
+
+        public async Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            var orderRm = OrderMapper.ToDTO(notification);
+            await _sqlDbRepository.InsertAsync(orderRm);
+        }
+    }
+}
